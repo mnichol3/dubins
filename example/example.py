ab0553a04b6f052527498722a6bc61f2a70636c1
mnichol3 <nicholson.matt94@gmail.com>
2025-05-24 15:53:21 -0400

    create example dir

---
+++ example/example.py
@@ -0,0 +1,15 @@
+"""Generate a Right-Straight-Left Dubins path."""
+from dubins import DubinsPath, Turn
+from point import Waypoint
+from plotting import plot_path
+
+
+origin = Waypoint(10, 0, 60)
+terminus = Waypoint(0, 4, 120)
+radius = 4
+turns = [Turn.RIGHT, Turn.LEFT]
+
+dub = DubinsPath(origin, terminus, radius, turns)
+points = dub.create_path(delta_psi=1, delta_d=0.5)
+
+plot_path(points, dub.circles)
